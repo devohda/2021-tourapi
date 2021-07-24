d7f86e2ac41fdec8a65ecf4e39b8d205d4dcad10
devohda <fffvvv6@gmail.com>
2021-07-25 00:34:56 +0900

    publish mypage screen

--- client/app/screens/MypageScreen.js
+++ client/app/screens/MypageScreen.js
@@ -9,14 +9,33 @@ import MyTabs from "../navigation/MypageNavigator";
 const MypageScreen = () => {
     return (
         <ScreenContainer>
-            <View style={{height: "30%"}} className="profile-container"></View>
+            <View style={{
+                height: "25%",
+                alignItems: "center",
+                paddingHorizontal: 10,
+                justifyContent: "space-between"
+            }} className="profile-container" flexDirection="row">
+                <View className="profile-img-container">
+                    <Image style={{width: 120, height: 120, borderRadius: 60}}
+                           source={require('../assets/images/suzy.jpeg')}
+                           resizeMode="cover"
+                    />
+                </View>
+                <View flex={1} style={{
+                    marginLeft: 30,
+                    height: "100%",
+                    justifyContent: "space-around",
+                    paddingVertical : 55
+                }}>
+                    <Text style={{fontSize : 20, fontWeight : "bold"}}>SuzyZzang!</Text>
+                    <Text>SuzyZzang~~</Text>
+                </View>
+            </View>
             <MyTabs/>
         </ScreenContainer>
     )
 };
 
-const styles = {
-
-}
+const styles = {}
 
 export default MypageScreen;
