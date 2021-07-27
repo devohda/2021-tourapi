5d89973fc56c69e491b4bfa1f8ab6874c703cacf
minsun <meeeeensun@gmail.com>
2021-07-27 19:50:49 +0900

    회원가입 넘어가는 것 빼고

--- client/app/navigation/LoginNavigator.js
+++ client/app/navigation/LoginNavigator.js
@@ -1,8 +1,12 @@
+//loginNavigator
+
 import React from 'react';
 import 'react-native-gesture-handler';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TouchableOpacity, View, BackHandler } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Icon } from 'react-native-elements';
+// import { NavigationAction } from '@react-navigation/native';
+import { NavigationActions } from 'react-navigation';
 
 import MainAuth from '../screens/MainAuth';
 import SubAuth from '../screens/SubAuth';
@@ -14,22 +18,29 @@ import NickNameAuth from '../screens/forSubAuth/NicknameAuth';
 
 const Stack = createStackNavigator()
 
-export default function LoginNavigator() {
-    return (
-        <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen name="Home" component={MainAuth} options={{headerShown: false}}/>
-            {/* 회원가입 back button 모양 수정 */}
-            <Stack.Screen name="SubAuth" component={SubAuth} options={{title: '회원가입', headerStyle: {elevation: 0}, headerTitleStyle: {fontSize: 18, marginStart: 100}}}/>
-            <Stack.Screen name="MainPage" component={MainPage} options={{title: '', headerStyle: {elevation: 0, backgroundColor:'black'},
-                headerLeft : () => <View><Text style={styles.headerLeft}>Here.</Text></View>,
-                headerRight : ()=>
-                    <View style={styles.headerRight}>
-                        <Icon type="ionicon" name={"md-search"} color="white" style={{marginEnd: 20}} size={28}></Icon>
-                        <Icon type="ionicon" name={"md-menu"} color="white" style={{marginEnd: 20}} size={28} ></Icon>
-                    </View>
-            }}/>
-        </Stack.Navigator>
-    );
+export default class LoginNavigator extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+    render() {
+        const {navigation} = this.props;
+        return (
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={MainAuth} options={{headerShown: false}}/>
+                <Stack.Screen name="SubAuth" component={SubAuth} options={{title: '회원가입', headerStyle: {elevation: 0}, headerTitleStyle: {fontSize: 18, marginStart: 100},
+                                                                                            headerLeft: () => <TouchableOpacity style={{left: 16}} ><Icon type="ionicon" name={"chevron-back-outline"} size={28} onPress={()=>navigation.goBack()}></Icon></TouchableOpacity>}}/>
+                <Stack.Screen name="MainPage" component={MainPage} options={{title: '', headerStyle: {elevation: 0, backgroundColor:'black'},
+                    headerLeft : () => <View><Text style={styles.headerLeft}>Here.</Text></View>,
+                    headerRight : ()=>
+                        <View style={styles.headerRight}>
+                            <Icon type="ionicon" name={"md-search"} color="white" style={{marginEnd: 20}} size={28}></Icon>
+                            {/* <Icon type="ionicon" name={"md-menu"} color="white" style={{marginEnd: 20}} size={28} ></Icon> */}
+                        </View>
+                }}/>
+            </Stack.Navigator>
+        );
+    }
+
 }
 
 const styles = StyleSheet.create({
--- client/app/screens/SubAuth.js
+++ client/app/screens/SubAuth.js
@@ -1,4 +1,6 @@
-import React from 'react';
+/////////
+//SubAuth.js
+import React, { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createStackNavigator, HeaderBackButton } from '@react-navigation/stack';
@@ -11,11 +13,37 @@ const NavigationTheme = {
     background: 'transparent'
   },
 };
+const onIncrease = () => {
+    setStep(step+1);
+    console.log(step)
+}
+const onDecrease = () => {
+    setStep(step-1)
+}
 
 export default class SubAuth extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {step: 0};
+    }
+    updateGrandparent(value) {
+        this.setState({step: value});
+    }
+    render() {
 
+        console.log(step);
+        return (
+            <ParentAuth key={this.state.step} updateParent={this.updateGrandparent.bind(this)}/>
+        )
+    }
+
+}
+class ParentAuth extends React.Component {
+    updateParent(value) {
+        this.props.updateGrandparent(value);
+    }
+    constructor(props) {
+        super(props);
         this.state = {
             forAuth : [{
                 question : '이메일',
@@ -37,56 +65,71 @@ export default class SubAuth extends React.Component {
             current: 0
         }
     }
-
+    data() {
+        return {
+        }
+    };
     componentDidMount() {
-        // this.setState({forProgress: this.state.forProgress.map(item => {
-        //     questions: item.
-        // })})
+
     }
-
     render() {
-
-        const progressComponent = this.state.forProgress.map(item => {
-            if(this.state.current == 0) {
-                return (
-                <View style={styles.button}>
-                    <View style={styles.yellowRect}></View>
-                    <View style={styles.grayRect}></View>
-                    <View style={styles.grayRect}></View>
-                </View>)
-            } else {
-                return (<View style={styles.grayRect}></View>)
-            }
-        })
-        // const questionComponent = this.state.forAuth[this.state.current]
-
-
+        // console.log(this.state.forAuth.map((card, index)=>{return card.question}))
         return (
             <>
-                <Text style={{display: 'none'}}>{this.state.current + 1}</Text>
-                <View style={styles.button}>
-                    <View style={styles.yellowRect}></View>
-                    <View style={styles.grayRect}></View>
-                    <View style={styles.grayRect}></View>
+                <View key={0}>
+                    <View style={styles.button}>
+                        <View style={styles.yellowRect}></View>
+                        <View style={styles.grayRect}></View>
+                        <View style={styles.grayRect}></View>
+                    </View>
+                    <AuthChild title="이메일" ad="을" placeholder="hiddenJewel@gmail.com" continue="계속하기" step={this.props.step} updateParent={this.updateParent.bind(this)}/>
                 </View>
-                    {/* {progressComponent} */}
-
-                <View style={styles.container}>
-                    <Text style={{fontSize: 28}}>
-                        <Text><Text>나만의 </Text><Text style={{fontWeight: "bold"}}>공간 보관함</Text><Text>을</Text></Text>
-                        <Text>{"\n"}채워볼까요?</Text>
-                    </Text>
-                <Text style={{fontSize: 16, marginTop: 67, marginBottom: 12}}><Text style={{fontWeight: "bold"}}>{this.state.forAuth[this.state.current].question}</Text><Text>를 입력해주세요</Text></Text>
-
-
-                    {/* placeholder 물어보기 */}
-                    <TextInput style={{fontSize: 16, borderBottomWidth: 1, borderBottomColor: '#C5C5C5', marginBottom: 27, paddingBottom: 11}} />                    
+                <View key={1}>
+                    <View style={styles.button}>
+                        <View style={styles.grayRect}></View>
+                        <View style={styles.yellowRect}></View>
+                        <View style={styles.grayRect}></View>
+                    </View>
+                    <AuthChild title="비밀번호" ad="를" placeholder="대/소문자 및 숫자 포함 8자리 이상" continue="계속하기" />
                 </View>
-                <TouchableOpacity style={{backgroundColor: '#DCDCDC', height: 52, borderRadius: 10, margin: 16, marginTop: 303}}><Text style={{textAlign: 'center', padding: 14, fontSize: 16, color: '#fff', fontWeight: 'bold'}}>계속하기</Text></TouchableOpacity>
-
+                <View key={2}>
+                    <View style={styles.button}>
+                        <View style={styles.grayRect}></View>
+                        <View style={styles.grayRect}></View>
+                        <View style={styles.yellowRect}></View>
+                    </View>
+                    <AuthChild title="닉네임" ad="을" placeholder="2자 이상 10자 미만으로 한글, 영문 대소문자만 가능" continue="가입완료" />
+                </View>
            </>
           );
-        }
+    }
+}
+
+class AuthChild extends React.Component {
+    handleClick(event) {
+        this.props.updateParent(this.props.step + 1);
+    }
+    render() {
+        return (
+            <>
+
+                    <View style={styles.container}>
+                        <Text style={{fontSize: 28}}>
+                            <Text><Text>나만의 </Text><Text style={{fontWeight: "bold"}}>공간 보관함</Text><Text>을</Text></Text>
+                            <Text>{"\n"}채워볼까요?</Text>
+                        </Text>
+                        <Text style={{fontSize: 16, marginTop: 67, marginBottom: 12}} ><Text style={{fontWeight: "bold"}}>{this.props.title}</Text><Text>{this.props.ad} 입력해주세요</Text></Text>
+
+                        {/* placeholder 물어보기 */}
+                        {/* → email : hiddenJewel@gmail.com
+                        → 닉네임 : 2자 이상 10자 미만으로 한글, 영문 대소문자만 가능 */}
+                        <TextInput style={{fontSize: 16, borderBottomWidth: 1, borderBottomColor: '#C5C5C5', marginBottom: 27, paddingBottom: 11}} placeholder={this.props.placeholder}/>                    
+                    </View>
+                    <TouchableOpacity style={{backgroundColor: '#DCDCDC', height: 52, borderRadius: 10, margin: 16, marginTop: 303}} onPress={()=>this.handleClick.bind(this)}><Text style={{textAlign: 'center', padding: 14, fontSize: 16, color: '#fff', fontWeight: 'bold'}}>{this.props.continue}</Text></TouchableOpacity>
+
+            </>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
