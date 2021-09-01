0af5c75fde042faf4481fda44db4ffd79ccf0e5c
devohda <fffvvv6@gmail.com>
2021-09-01 18:21:18 +0900

    free collection insert

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // 라우터 파일 가져오기
-const collectionRouter = require('./collections');
+const collectionRouter = require('./collection');
 const authRouter = require('./auth');
 const keywordRouter = require('./keywords');
 const userRouter = require('./users');
@@ -10,7 +10,7 @@ const tourApiRouter = require('./tourApi');
 
 
 // 라우터 파일 등록하기
-router.use('/collections', collectionRouter);
+router.use('/collection', collectionRouter);
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
 router.use('/keyword', keywordRouter);
