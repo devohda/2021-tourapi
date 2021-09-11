66c0d36b581f3dc9fe131067f5ef3b1feb229cfe
minsun <meeeeensun@gmail.com>
2021-09-11 20:50:37 +0900

    package edited

--- client/app/components/NavigationTop.js
+++ client/app/components/NavigationTop.js
@@ -3,8 +3,8 @@ import {Image, Platform, TouchableOpacity, View} from "react-native";
 import {useTheme} from '@react-navigation/native';
 import AppText from "./AppText";
 import BackIcon from '../assets/images/back-icon.svg';
-import { Menu, Provider, Divider, Appbar } from 'react-native-paper';
-import { Icon } from 'react-native-elements';
+import {Menu, Provider, Divider, Appbar} from 'react-native-paper';
+import {Icon} from 'react-native-elements';
 
 // ** customize 한 스택 네비게이션 헤더 입니다.
 // ** props 로 navigation(navigation 객체), title(String) 을 받습니다.
@@ -19,23 +19,21 @@ const NavigationTop = props => {
         <View flexDirection="row" style={{
             height: 24,
             marginBottom: 20,
-            marginVertical: Platform.OS === 'ios' ? 20 : 0,
-            marginHorizontal: 5,
+            marginTop: Platform.OS === 'ios' ? 20 : 0,
+            marginHorizontal: 20,
             alignItems: 'center',
             justifyContent: 'center'
         }}>
-            <View style={{position: 'absolute', left: 10}}>
-                <TouchableOpacity onPress={() => {
-                    props.navigation.goBack()
-                }}>
-                    <BackIcon width={24} height={24} style={{color : colors.mainColor}}/>
+            <View style={{position: 'absolute', left: 0}}>
+                <TouchableOpacity onPress={() => props.navigation.goBack()}>
+                    <BackIcon width={24} height={24} style={{color: colors.mainColor}}/>
                 </TouchableOpacity>
             </View>
             <AppText style={{color: colors.mainColor, fontSize: 16, fontWeight: 'bold'}}>
                 {props.title}
             </AppText>
             {/* <View style={{position: 'absolute', right: 0}}> */}
-                {/* {props.type === 'freeDir' && <MenuComponent />} */}
+            {/* {props.type === 'freeDir' && <MenuComponent />} */}
             {/* </View> */}
         </View>
     )
