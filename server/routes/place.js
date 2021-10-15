0d46d6f493c09ed622956041507068f5ce962396
devohda <fffvvv6@gmail.com>
2021-10-15 17:29:29 +0900

    multiple file upload

--- server/routes/place.js
+++ server/routes/place.js
@@ -6,6 +6,20 @@ const reviewService = require('../services/reviewService');
 const {verifyToken} = require("../middleware/jwt");
 const {readPlaceList} = require("../services/placeService");
 
+const Multer = require('multer');
+const {Storage} = require('@google-cloud/storage');
+const collectionService = require("../services/collectionService");
+
+const projectId = 'here-327421'
+const keyFilename = 'here-327421-e0bed35f44b5.json'
+const storage = new Storage({projectId, keyFilename});
+
+const multer = Multer({
+    storage: Multer.memoryStorage()
+});
+
+const bucket = storage.bucket('here-bucket');
+
 // [READ]
 // 장소 리스트
 router.get('/list', verifyToken, async (req, res, next) => {
@@ -75,31 +89,69 @@ router.get('/:placeId/comments', async (req, res, next) => {
 
 //[CREATE]
 // 장소 리뷰 생성
-router.post('/:placeId/review', verifyToken, async (req, res, next) => {
+router.post('/:placeId/review', verifyToken, multer.array('img', 5), async (req, res, next) => {
     const {reviewData} = req.body;
     const place_pk = req.params.placeId
     const {user} = res.locals;
     const {review_facility, ...reviewData2} = reviewData;
-    try {
+
+    if (!req.files) {
         const review = {
             user_pk : user.user_pk,
             place_pk : place_pk,
             ...reviewData2
         }
-        console.log(review_facility)
 
-        await reviewService.createReview(review, review_facility);
-    } catch (err) {
-        return res.status(500).json({
-            code: 500,
-            status: 'SERVER ERROR'
-        });
+        const result = await reviewService.createReview(review, review_facility);
+
+        if(result){
+            return res.status(200).json({
+                code: 200,
+                status: 'OK'
+            });
+        }else{
+            return res.status(500).json({
+                code: 500,
+                status: 'SERVER ERROR'
+            });
+        }
     }
 
-    return res.status(200).json({
-        code: 200,
-        status: 'OK'
+    for(const file of req.files){
+        const blob = bucket.file(Date.now() + req.file.originalname);
+        const blobStream = blob.createWriteStream();
+    }
+
+    blobStream.on('error', err => {
+        next(err);
     });
+
+    blobStream.on('finish', async () => {
+        // The public URL can be used to directly access the file via HTTP.
+        const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+
+        const {collectionData: stringedCollectionData} = req.body;
+        const collectionData = {
+            ...JSON.parse(stringedCollectionData),
+            img: publicUrl
+        }
+        const result = await collectionService.createFreeCollection(user.user_pk, collectionData)
+
+        if (result.collection_pk) {
+            return res.status(200).json({
+                code: 200,
+                status: 'OK',
+                collectionId: result.collection_pk
+            });
+        } else {
+            return res.status(500).json({
+                code: 500,
+                status: 'SERVER ERROR'
+            });
+        }
+    });
+
+    blobStream.end(req.file.buffer);
 });
 
 module.exports = router;
