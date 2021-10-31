cf6313c63a084bfbf70b6bc8467c01bebcf32a69
minsun <meeeeensun@gmail.com>
2021-10-31 09:50:11 +0900

    마이페이지 렌더링 수정

--- client/app/screens/mypage/placeTab.js
+++ client/app/screens/mypage/placeTab.js
@@ -40,7 +40,10 @@ const PlaceTab = ({navigation}) => {
     const [currentRendering, setCurrentRendering] = useState(0);
 
     useEffect(() => {
+        getLikedPlace('RESENT');
+        getLikedCollection('RESENT');
         setShowMenu(false);
+        setCurrentMenu('최근 추가순');
     }, [isFocused]);
 
     const getLikedPlace = (NOW) => {
